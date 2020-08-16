e83f7f29dd3c23d6c4fda95ace399913ee699797
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-08-16 15:25:35 -0500

    product routes done

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,45 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Category
+      },
+      {
+        model: Tag,
+        through: ProductTag
+      }
+    ]
+  })
+  .then((dbProductData) => res.json(dbProductData))
+    .catch((err) => {
+      res.status(500).json(err);
+    })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Category
+      },
+      {
+        model: Tag,
+        through: ProductTag
+      }
+    ]
+  })
+  .then((dbProductData) => res.json(dbProductData))
+    .catch((err) => {
+      res.status(500).json(err);
+    })
 });
 
 // create new product
@@ -91,6 +124,15 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((dbProductData) => res.json(dbProductData))
+    .catch((err) => {
+      res.status(500).json(err);
+    })
 });
 
 module.exports = router;

c232a2af14aa38e822bd12fe79d8fedd1a455c65
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-08-16 20:24:24 -0500

    tag route created

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,28 +1,79 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
+const { Model } = require('sequelize/types');
 
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [
+      {
+      Model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock']
+      }
+    ]
+  })
+  .then((dbProductData) => res.json(dbProductData))
+    .catch((err) => {
+      res.status(500).json(err);
+    })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+      Model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock']
+      }
+    ]
+  })
+  .then((dbProductData) => res.json(dbProductData))
+    .catch((err) => {
+      res.status(500).json(err);
+    })
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+  .then((dbProductData) => res.json(dbProductData))
+  .catch((err) => {
+    res.status(500).json(err);
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({
+    where: {
+      id: req.params.id
+    },
+  })
+  .then((dbProductData) => res.json(dbProductData))
+    .catch((err) => {
+      res.status(500).json(err);
+    })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    },
+  })
+  .then((dbProductData) => res.json(dbProductData))
+    .catch((err) => {
+      res.status(500).json(err);
+    })
 });
 
 module.exports = router;

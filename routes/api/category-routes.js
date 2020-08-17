d5574a8fb671f5920c03e6d54cbe1dbb7ae8903f
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-08-16 21:18:21 -0500

    troubleshooting tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
-const { findOne } = require('../../models/Tag');
 
 // The `/api/categories` endpoint
 
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
-const { Model } = require('sequelize/types');
+const { param } = require('./category-routes');
 
 // The `/api/tags` endpoint
 
